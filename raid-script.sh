16160495ba6cc98b1a54f9778538cceee055ced2
stoertebeker2k <schramm.norbert@t-online.de>
2015-08-16 23:23:45 +0200

    usable raid-script

--- raid-script.sh
+++ raid-script.sh
@@ -14,15 +14,15 @@ username="stoertebeker"
 
 if [ -z "$1" ]
 then
-	echo kein RAID-Typ angegeben!
-	echo Korrekte Verwendung: ./raid-script.sh RAID-Typ Anzahl_Festplatten
+	echo "kein RAID-Typ angegeben!"
+	echo "Korrekte Verwendung: ./raid-script.sh RAID-Typ Anzahl_Festplatten"
 	exit 1
 fi
 
 if [ -z "$2" ]
 then
-        echo Keine Festplattenanzahl angegeben!
-        echo Korrekte Verwendung: ./raid-script.sh RAID-Typ Anzahl_Festplatten
+        echo "Keine Festplattenanzahl angegeben!"
+        echo "Korrekte Verwendung: ./raid-script.sh RAID-Typ Anzahl_Festplatten"
         exit 1
 fi
 
@@ -35,45 +35,45 @@ sleep 1
 ## Partitonstabelle erstellen
 for ((i=0;i<"$2";i++))
 do
-        echo parted "${devices[$i]}" mklabel gpt
+        parted -s "${devices[$i]}" mklabel gpt
 done
 
 ## Partitionen erstellen
 for ((i=0;i<"$2";i++))
 do
-        echo parted -a optimal -- "${devices[$i]}" mkpart primary 2048s -8192s
+        parted -a optimal -- "${devices[$i]}" mkpart primary 2048s -8192s
 done
 
 ## Partition als RAID-Partition markieren
 for ((i=0;i<"$2";i++))
 do
-        echo parted "${devices[$i]}" set 1 raid on
+        parted "${devices[$i]}" set 1 raid on
 done
 
 ## Devices zusammenzählen
 for ((i=0;i<"$2";i++))
 do
-	devparms+=("${devices[$i]}")
+	devparms+=("${partitions[$i]}")
 done
 
 ## RAID erzeugen
-echo mdadm --create /dev/md0 --auto md --level=$1 --raid-devices=$2 ${devparms[@]}
+mdadm --create /dev/md0 --auto md --level=$1 --raid-devices=$2 ${devparms[@]}
 
 ## RAID5/6 Chunks
 #TODO
 
 ## Dateisystem erzeugen
-echo mkfs.ext4 /dev/md0 
+mkfs.ext4 /dev/md0 
 
 ## mounten
-echo mount /dev/md0 $mountpoint
-echo chown $username":"$username $mountpoint
+mount /dev/md0 $mountpoint
+chown $username":"$username $mountpoint
 
 ### Warteschleife, bis RAID fertig initialisiert ist
 fertig=0
 
 ##Initialisieren beschleunigen
-echo "echo 200000 > /proc/sys/dev/raid/speed_limit_min"
+echo 200000 > /proc/sys/dev/raid/speed_limit_min
 
 while [ $fertig -eq 0 ]
 do
@@ -83,25 +83,23 @@ if [ "$status" == "clean" ]
 	then
 		echo "RAID clean!"
 		sleep 5
-		status="$(grep -Po "(?<=State : ).*(?=.)" < <(sudo mdadm -D /dev/md0))"
 		fertig=1
 	else
 		echo "processing"
-		status="$(grep -Po "(?<=State : ).*(?=.)" < <(sudo mdadm -D /dev/md0))"
 		sleep 5
 fi
 done
 ##Beschleunigung für Initialisierung zurücksetzen
-echo "echo 1000 > /proc/sys/dev/raid/speed_limit_min"
+echo 1000 > /proc/sys/dev/raid/speed_limit_min
 
 ## Benchmark
 echo Benchmark ...
-#fio-bench.sh raid$1-devices$2
+./fio-bench.sh raid$1-devices$2
 sleep 2
 
 ### RAID abbauen
-echo umount /dev/md0
-echo mdadm --stop /dev/md0 
+umount /dev/md0
+mdadm --stop /dev/md0 
 
 ##Superblöcke löschen
-echo mdadm --zero-superblock ${devparms[@]}
+mdadm --zero-superblock ${devparms[@]}
